9e962686b339ec0f0cef03f24e65f9d2d9875449
Rodrigoaaalves <136371023+Rodrigoaaalves@users.noreply.github.com>
2023-10-25 15:11:28 -0300

    Conta.java

---
+++ Conta.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Conta {
+    private String agencia;
+    private String numeroConta;
+    private double saldo;
+    private List<String> transacoes;
+
+    public Conta(String agencia, String numeroConta, double saldo, Cliente cliente) {
+        this.agencia = agencia;
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+        this.transacoes = new ArrayList<>();
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+        transacoes.add("Depósito: +" + valor);
+    }
+
+    public void sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+            transacoes.add("Saque: -" + valor);
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
+
+    public void transferir(double valor, Conta contaDestino) {
+        if (valor <= saldo) {
+            saldo -= valor;
+            transacoes.add("Transferência: -" + valor + " para conta " + contaDestino.numeroConta);
+            contaDestino.depositar(valor);
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
+
+    public List<String> extrato() {
+        return transacoes;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Agência: " + agencia + ", Número da Conta: " + numeroConta + ", Saldo: " + saldo;
+    }
+}
